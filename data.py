71835070b83fccad310c3765f52c94c4034e1d93
Alexander Delargy <59781025+alexdelargy@users.noreply.github.com>
2026-04-21 16:46:57 -0400

    Update data.py

--- data.py
+++ data.py
@@ -13,7 +13,7 @@ EARTH_RADIUS = 6371e3
 
 def getLocation():
     # lat, lon = geocoder.ip('me').latlng
-    return [38.8964, -77.1606]
+    return [38.9550, -77.45105]
 
 def getBounds(radius_miles: float) -> Tuple[float, float, float, float]:
     """
